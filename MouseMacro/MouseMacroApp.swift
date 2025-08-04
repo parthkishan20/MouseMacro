5e77b08d71d22498623c9c9048c8854e73495144
Parthkumar Patel <parthkishan20@gmail.com>
2025-08-04 15:02:19 -0400

    Initial Commit

---
+++ MouseMacro/MouseMacroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MouseMacroApp.swift
+//  MouseMacro
+//
+//  Created by SkyBolt on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MouseMacroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
